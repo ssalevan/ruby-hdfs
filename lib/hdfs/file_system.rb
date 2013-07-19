2a52787c178499f655056d3b987644cd04be08bb
Steve Salevan <steve@tumblr.com>
2013-07-19 13:54:22 -0400

    Fixing walk

--- lib/hdfs/file_system.rb
+++ lib/hdfs/file_system.rb
@@ -51,7 +51,7 @@ module HDFS
           :directories => []}) do |memo, item|
         if item.is_directory?
           memo[:directories] << item
-          yield walk item.name
+          walk item.name
         else
           memo[:files] << item
         end
